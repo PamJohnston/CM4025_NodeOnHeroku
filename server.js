5d4bfa9cab63c3936be5c0ff8ffab46c3879f3ab
Pam Johnston <1609098@rgu.ac.uk>
2021-02-02 16:28:39 +0000

    naughty names

--- server.js
+++ server.js
@@ -31,6 +31,13 @@ adminRouter.get('/', function(req, res) {
 // users page (http://localhost:PORT/admin/users) 
 adminRouter.get('/users', function(req, res) {
   res.send('I show all the users!');  });
+
+// route middleware to validate :name 
+adminRouter.param('name', function(req, res, next, name) {   // do validation on name here   // log something so we know its working 
+  console.log('doing name validations on ' + name);   // once validation is done save the new item in the req   req.name = name;   // go to the next thing    next(); 
+});
+
+
 // route with parameters (http://localhost:PORT/admin/users/:name) 
 adminRouter.get('/users/:name', function(req, res) {   res.send('hello ' + req.params.name + '!');  });  
 
