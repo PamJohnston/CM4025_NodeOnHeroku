6c27a9848122dd44a87d7eb7466c9b3ad2d4bb2d
Pam Johnston <1609098@rgu.ac.uk>
2021-02-02 16:00:14 +0000

    adding middleware

--- server.js
+++ server.js
@@ -28,6 +28,16 @@ adminRouter.get('/posts', function(req, res) {
 app.use('/admin', adminRouter); 
 ///////////////////////////////////////////////////////////////////
 
+///////////////////////////////////////////////////////////////////
+// route middleware that will happen on every request
+  adminRouter.use(function(req, res, next) {
+    // log each request to the console
+    console.log(req.method, req.url);
+    // continue doing what we were doing and go to the route
+    next(); });
+///////////////////////////////////////////////////////////////////
+
+
 
 // start the server
 app.listen(PORT);
