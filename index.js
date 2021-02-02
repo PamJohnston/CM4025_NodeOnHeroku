d337cc9c3673fd8d0a97092cb0278d498cc00622
Pam Johnston <1609098@rgu.ac.uk>
2021-02-02 14:38:46 +0000

    changing ports

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var http = require("http");
 
+const PORT = process.env.PORT || 8080;
+
 http.createServer(function (request, response) {
    // Send the HTTP header
    // HTTP Status: 200 : OK
@@ -8,7 +10,7 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8000);
+}).listen(PORT);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8000/');
+console.log('Server running at http://127.0.0.1:'+PORT);
